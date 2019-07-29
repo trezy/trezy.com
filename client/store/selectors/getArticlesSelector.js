0665ecba69102963ec51bb0918ff349c62145282
Trezy <tre@trezy.com>
2019-07-29 13:49:09 -0500

    fix: handle draft articles when sorting

--- client/store/selectors/getArticlesSelector.js
+++ client/store/selectors/getArticlesSelector.js
@@ -4,23 +4,37 @@ import moment from 'moment'
 
 
 
-const getArticlesSelector = () => ({ articles }) => {
-  const articleList = Object.values(articles)
-
-  return articleList.sort((articleA, articleB) => {
-    const publishDateA = moment(articleA.publishedAt.seconds * 1000)
-    const publishDateB = moment(articleB.publishedAt.seconds * 1000)
-
-    if (publishDateA < publishDateB) {
+const sortArticles = (articleA, articleB) => {
+  if (!articleA.publishedAt || !articleB.publishedAt) {
+    if (Boolean(articleA.publishedAt) && !articleB.publishedAt) {
       return 1
     }
 
-    if (publishDateA > publishDateB) {
+    if (!articleA.publishedAt && Boolean(articleB.publishedAt)) {
       return -1
     }
 
     return 0
-  })
+  }
+
+  const publishDateA = moment(articleA.publishedAt.seconds * 1000)
+  const publishDateB = moment(articleB.publishedAt.seconds * 1000)
+
+  if (publishDateA < publishDateB) {
+    return 1
+  }
+
+  if (publishDateA > publishDateB) {
+    return -1
+  }
+
+  return 0
+}
+
+const getArticlesSelector = () => ({ articles }) => {
+  const articleList = Object.values(articles)
+
+  return articleList.sort(sortArticles)
 }
 
 
