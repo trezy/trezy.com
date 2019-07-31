66aaeddbae3d7a6bcd62204eb5e34487ee3aa58e
Trezy <tre@trezy.com>
2019-07-31 11:58:37 -0500

    feat(/): add a hero to the homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,6 +6,7 @@ import React from 'react'
 
 
 // Component imports
+import { Link } from '../routes'
 import ArticleList from '../components/ArticleList'
 import ClientList from '../components/ClientList'
 import PageWrapper from '../components/PageWrapper'
@@ -16,16 +17,28 @@ import PageWrapper from '../components/PageWrapper'
 
 const Home = () => (
   <PageWrapper title="Home">
+    <section className="hero">
+      <header>
+        &lt;trezy-who /&gt;
+      </header>
+
+      <p>Software engineer, UX designer, and <a href="https://a11yproject.com">#a11y</a>.</p>
+    </section>
+
     <section>
       <div>
-        <header>Latest article</header>
+        <header>Latest articles</header>
 
-        <ArticleList limit={1} />
+        <ArticleList limit={3} />
+
+        <Link route="blog">
+          <a>See more</a>
+        </Link>
       </div>
     </section>
 
-    <section className="hero">
-      <header>Trusted by:</header>
+    <section className="center">
+      <p>As a software consultant, I've been lucky to work with some of the coolest teams from well-known brands on products that make the world a better place.</p>
 
       <ClientList />
     </section>
