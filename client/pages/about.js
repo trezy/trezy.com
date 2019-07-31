e78ef1e6839f0b4d160f7d7322498b9998b8fabe
Trezy <tre@trezy.com>
2019-07-31 11:57:23 -0500

    feat(/about): update the about page

--- client/pages/about.js
+++ client/pages/about.js
@@ -49,7 +49,7 @@ const About = () => (
           data-animation="fade-in-from-bottom-small"
           data-animation-delay="1.5s"
           data-animation-duration="1s">
-            I'm a wannabe cyborg and software engineer raised in the luscious forests of the midwest US. I've been writing code since I was 12 when my mother bought me my first technology book: <cite><a href="https://www.amazon.com/dp/0764585304">CliffsNotes: Creating Web Pages with HTML</a></cite>. I subsequently created my very first website, which was a Pokémon-based choose-your-own-adventure.
+            I'm a software engineer and a wannabe cyborg. I was born in the flatlands of Texas, raised among the luscious forests of the Shawnee National Forest, and I eventually found my way to the rolling hills of Wisconsin. My mother bought me my first technology book, <cite><a href="https://www.amazon.com/dp/0764585304">CliffsNotes: Creating Web Pages with HTML</a></cite>, when I was 12 and I've been writing code ever since. I subsequently created my very first website: a Pokémon-based choose-your-own-adventure.
         </p>
 
         <p
@@ -57,7 +57,7 @@ const About = () => (
           data-animation="fade-in-from-bottom-small"
           data-animation-delay="1.8s"
           data-animation-duration="1s">
-          My web dev hobby quickly became a full-time career in which I've been fortunate enough to work with companies like <a href="https://amazon.com">Amazon</a>, <a href="https://disney.com">Disney</a>, <a href="https://eventbrite.com">Eventbrite</a>, <a href="https://gettyimages.com">Getty Images</a>, and <a href="https://gopro.com">GoPro</a>. I've also been involved with a handful of <abbr title="Free Open Source Software">FOSS</abbr> projects used by hundreds of thousands of projects all over the world.
+          My web dev hobby quickly became a full-time career. I've been fortunate enough to work with companies like <a href="https://amazon.com">Amazon</a>, <a href="https://disney.com">Disney</a>, <a href="https://eventbrite.com">Eventbrite</a>, <a href="https://gettyimages.com">Getty Images</a>, and <a href="https://gopro.com">GoPro</a>. I've also been involved with a handful of <abbr title="Free Open Source Software">FOSS</abbr> projects used by hundreds of thousands of projects all over the world.
         </p>
 
         <p
