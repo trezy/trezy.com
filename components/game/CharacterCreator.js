afb434bd183e8b03754570b6e41e2d75a4c72183
Trezy <tre@trezy.com>
2019-09-24 22:39:07 -0500

    fix(<CharacterCreator>): ensure characters are created with gender

--- components/game/CharacterCreator.js
+++ components/game/CharacterCreator.js
@@ -39,9 +39,10 @@ const CharacterCreator = ({ onSubmit, ownerID }) => {
       profession,
       color,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      gender,
       name,
       ownerID,
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
       x: 0,
       y: 0,
     })
