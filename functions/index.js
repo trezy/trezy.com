ff359b71d23e9c25bf52b390a88619618f01de2d
Trezy <tre@trezy.com>
2020-01-31 01:06:40 -0600

    feat: update user claims when roles are changed

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,68 @@
-const functions = require('firebase-functions')
+// Module imports
 const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const uuid = require('uuid')
+const isEqual = require('lodash/isEqual')
+const xor = require('lodash/xor')
+
+
+
+
+
+// Firebase setup
 admin.initializeApp()
 
+const auth = admin.auth()
 const firestore = admin.firestore()
 
-exports.onUserStatusChanged = functions.database.ref('game/characters/{id}/status').onUpdate(async (change, context) => {
-  const eventStatus = change.after.val()
-  const userStatusFirestoreRef = firestore.doc(`characters/${context.params.id}`)
-  const statusSnapshot = await change.after.ref.once('value')
-  const status = statusSnapshot.val()
 
-  if (status.updatedAt > eventStatus.updatedAt) {
-    return null
+
+
+
+// Helpers
+const updateUserClaims = async snapshot => {
+  const {
+    roles: userRoles,
+  } = snapshot.data()
+  const { customClaims } = await auth.getUser(snapshot.id)
+  let newClaims = {}
+
+  const rolesSnapshot = await firestore.collection('roles').get()
+  const allRoles = {}
+  rolesSnapshot.forEach(role => {
+    allRoles[role.id] = role.data().claims
+  })
+
+  Object.entries(allRoles).forEach(([role, claims]) => {
+    if (userRoles.includes(role)) {
+      claims.forEach(claim => {
+        newClaims[claim] = true
+      })
+    }
+  })
+
+  if (!isEqual(customClaims, newClaims)) {
+    console.log(`Updating claims:`, newClaims)
+    await auth.setCustomUserClaims(snapshot.id, newClaims)
+    await firestore.collection('users').doc(snapshot.id).update({ refreshToken: uuid() })
+    console.log(`Done.`)
+  } else {
+    console.log(`Claims have not changed.`)
   }
+}
 
-  eventStatus.updatedAt = new Date(eventStatus.updatedAt)
 
-  return userStatusFirestoreRef.update({ active: eventStatus.active })
+
+
+
+// Handlers
+exports.onUserCreate = functions.firestore.document('users/{userID}').onCreate(updateUserClaims)
+
+exports.onUserUpdate = functions.firestore.document('users/{userID}').onUpdate((snapshot, context) => {
+  const oldRoles = snapshot.before.data().roles
+  const newRoles = snapshot.after.data().roles
+
+  if (xor(oldRoles, newRoles).length) {
+    updateUserClaims(snapshot.after)
+  }
 })
