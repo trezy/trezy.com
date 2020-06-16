f8be0072e19a1c1547e940e1e7073a7fab11a5b6
Trezy <tre@trezy.com>
2020-06-16 12:04:48 -0500

    feat(/experience): update career history

--- pages/experience.js
+++ pages/experience.js
@@ -19,18 +19,77 @@ import PageWrapper from '../components/PageWrapper'
 const data = [
   {
     employer: {
+      name: 'Webflow',
+      url: 'http://webflow.com/',
+    },
+    id: uuid(),
+    positions: [
+      {
+        accomplishments: [
+          'Rearchitected Tracktastic from the ground up',
+          'Fixed numerous memory leaks, bringing the memory footprint to consistently under 20MB from its previous 150MB/hour',
+          'Designed and developed a new web interface to augment the UX of Tracktastic\'s chat interface',
+        ],
+        description: `I assumed the responsibilities of architecting and developing the company\'s internal status tracking chat bot, Tracktastic. The bot began as a hack job, described by its developer as, "a raging dumpster fire that just needed to work."
+
+I took the bot from a very basic Slack app to an integrated API with well-architected data structures, a companion web app, and a substantially improved UX.`,
+        id: uuid(),
+        location: 'Madison, WI',
+        startDate: 'August 2019',
+        endDate: 'June 2020',
+        tech: [
+          'Angular.js',
+          'Buildkite',
+          'Docker',
+          'Express.js',
+          'Flow',
+          'Jade/Pug',
+          'JavaScript',
+          'MongoDB',
+          'Mongoose.js',
+          'Next.js',
+          'React.js',
+          'Redux',
+          'Sass',
+          'TypeScript',
+        ],
+        title: 'Senior Software Engineer',
+      },
+    ],
+  },
+
+  {
+    employer: {
       name: 'Mobile Doorman',
       url: 'http://mobiledoorman.com/',
     },
     id: uuid(),
     positions: [
       {
-        description: `
-**Tech:** Babel, CircleCI, CodeShip, ESLint, Javascript, Nuxt.js, PostCSS, Ruby, Rails, Sass, Vue.js, Webpack`,
+        accomplishments: [
+          'Lead weekly triage meetings with engineers and QA, making our way down the list of tier 3 bug reports that nobody had been able to crack alone',
+          'Rearchitected our messaging core data structures, unlocking the potential for hundreds of new features and removing the need for engineers to dance around the former monolithic constructs',
+        ],
+        description: 'My directive at Mobile Doorman was to lead-by-example and elevate a team of very talented engineers from junior to mid-level. I accomplished this by regularly taking initiative on new projects, as well as diving into projects that had been long-neglected.',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'February 2019',
         endDate: 'August 2019',
+        tech: [
+          'Babel',
+          'CircleCI',
+          'CodeShip',
+          'ESLint',
+          'Javascript',
+          'Next.js',
+          'Nuxt.js',
+          'PostCSS',
+          'React.js',
+          'Ruby on Rails',
+          'Sass',
+          'Vue.js',
+          'Webpack',
+        ],
         title: 'Senior Software Engineer',
       },
     ],
@@ -44,38 +103,74 @@ const data = [
     id: uuid(),
     positions: [
       {
-        description: `At Roll For Guild, we're working to make it easier for tabletop roleplaying gamers to find each other. As the CTO, I lead our engineering team, vet potential engineering hires, and adjust the agile process.
-
-**Accomplishments:**
-* Launched a fully-functional social network in under 3 months
-* Grew a user base of more than 400 users in less than 2 months
-* Implemented processes for constant improvement of our agile workflow
-
-**Tech:** Babel, Echarts, Elixir, Enzyme, ESLint, Javascript, Jest, Koa.js, Next.js, Phoenix, React.js, Redux.js, Webpack`,
+        accomplishments: [
+          'Launched a fully-functional social network in under 3 months',
+          'Grew a user base of more than 400 users in less than 2 months',
+          'Implemented processes for constant improvement of our agile workflow',
+        ],
+        description: 'At Roll For Guild, our goal was to make it easier for tabletop roleplaying gamers to find each other. As the CTO, I lead our engineering team, vetted potential engineering hires, and managed and tweaked our agile process.',
         endDate: 'August 2018',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'October 2017',
-        title: 'Chief Technology Officer (CTO)',
+        tech: [
+          'Babel',
+          'Echarts',
+          'Elixir',
+          'Enzyme',
+          'ESLint',
+          'Javascript',
+          'Jest',
+          'Koa.js',
+          'Next.js',
+          'Phoenix',
+          'React.js',
+          'Redux.js',
+          'Webpack',
+        ],
+        title: 'Chief Technology Officer',
       },
     ],
   },
 
   {
     employer: {
-      name: 'Freelance Software Consultant',
+      name: 'Trezy.com',
       url: 'http://trezy.com/',
     },
     id: uuid(),
     positions: [
       {
-        description: `As a consultant, I've helped companies from all over the world take their engineering teams from dysfunctional to superior. I help companies by embedding with their engineering teams and coaching them through developing better bonds, streamlining their agile process, and better engaging with their mission.
-
-**Tech:** Angular.js, Ansible, Babel, Backbone.js, Backbone.Marionette.js, CircleCI, Docker, ESLint, Enzyme, Express.js, Grunt, Koa.js, Javascript, Jest, Next.js, PostCSS, React.js, Redux, Sass, TravisCI, Vagrant, Webpack`,
+        accomplishments: [],
+        description: 'As a consultant, I\'ve helped companies from all over the world take their engineering teams from dysfunctional to superior. I help companies by embedding with their engineering teams and coaching them through developing better bonds, streamlining their agile process, and better engaging with their mission.',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'January 2017',
-        title: 'Senior Software Engineer',
+        tech: [
+          'Angular.js',
+          'Ansible',
+          'Babel',
+          'Backbone.js',
+          'Backbone.Marionette.js',
+          'CircleCI',
+          'Docker',
+          'ESLint',
+          'Enzyme',
+          'Express.js',
+          'Grunt',
+          'Koa.js',
+          'Javascript',
+          'Jest',
+          'Next.js',
+          'PostCSS',
+          'React.js',
+          'Redux',
+          'Sass',
+          'TravisCI',
+          'Vagrant',
+          'Webpack',
+        ],
+        title: 'Software Engineering Consultant',
       },
     ],
   },
@@ -88,16 +183,35 @@ const data = [
     id: uuid(),
     positions: [
       {
-        description: `As a consultant with Adorable, I worked closely with our clients to help them architect and develop applications, as well as coaching them through the process of implementing and revising agile workflows. I also mentored junior engineers to get them up-to-speed and ready to take on full projects.
-
-**Accomplishments:**
-* Increased client's engineering team engagement by 40% by coaching them through revising their agile process
-
-**Tech:** Alt.js, Angular.js, Babel, CircleCI, Cucumber, D3.js, Enzyme, ESLint, Express.js, Grunt, Gulp, Javascript, Jest, Koa.js, Next.js, React.js, Redux.js, TravisCI, Webpack`,
+        accomplishments: [
+          'Increased client\'s engineering team engagement by 40% by coaching them through revising their agile process (measured by post-contract employee survey)',
+        ],
+        description: 'As a consultant with Adorable, I worked closely with our clients to help them architect and develop applications, as well as coaching them through the process of implementing and revising agile workflows. I also mentored junior engineers to get them up-to-speed and ready to take on full projects.',
         endDate: 'February 2017',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'March 2016',
+        tech: [
+          'Alt.js',
+          'Angular.js',
+          'Babel',
+          'CircleCI',
+          'Cucumber',
+          'D3.js',
+          'Enzyme',
+          'ESLint',
+          'Express.js',
+          'Grunt',
+          'Gulp',
+          'Javascript',
+          'Jest',
+          'Koa.js',
+          'Next.js',
+          'React.js',
+          'Redux.js',
+          'TravisCI',
+          'Webpack',
+        ],
         title: 'Senior Software Engineer',
       },
     ],
@@ -111,17 +225,25 @@ const data = [
     id: uuid(),
     positions: [
       {
-        description: `At Interactive Intelligence my team was building a client-side voice, video, and text messaging application. I also mentored interns and junior engineers.
-
-**Accomplishments:**
-* Developed a 98% client-side realtime video chat platform with DVR-style capabilities (recording, rewind, resync, picture-in-picture) in less than a week
-* Developed a full VR prototype application in less than 2 days
-
-**Tech:** Angular.js, Chai, Coffeescript, Ember.js, Jasmine, Javascript, Mocha, Three.js`,
+        accomplishments: [
+          'Developed a 98% client-side realtime video chat platform with DVR-style capabilities (recording, rewind, resync, picture-in-picture) in less than a week',
+          'Developed a full VR prototype application in less than 2 days',
+        ],
+        description: 'At Interactive Intelligence my team was building a client-side voice, video, and text messaging application. I also mentored interns and junior engineers.',
         endDate: 'February 2016',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'July 2014',
+        tech: [
+          'Angular.js',
+          'Chai',
+          'Coffeescript',
+          'Ember.js',
+          'Jasmine',
+          'Javascript',
+          'Mocha',
+          'Three.js',
+        ],
         title: 'Software Engineer',
       },
     ],
@@ -135,15 +257,16 @@ const data = [
     id: uuid(),
     positions: [
       {
-        description: `My team at SHOPBOP/East Dane was tasked with maintenance of the current websites, as well as architecture and development of a new website.
-
-**Accomplishments:**
-* Developed a library for XSS protection
-* Developed a library for advanced client-side form validations`,
+        accomplishments: [
+          'Developed a library for XSS protection',
+          'Developed a library for advanced client-side form validations',
+        ],
+        description: 'My team at SHOPBOP/East Dane was tasked with maintenance of the current websites, as well as architecture and development of a new website.',
         endDate: 'July 2014',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'February 2014',
+        tech: [],
         title: 'Front End Developer',
       },
     ],
@@ -157,13 +280,20 @@ const data = [
     id: uuid(),
     positions: [
       {
-        description: `My responsibilities at Great Wolf included maintaining and upgrading the company website and working with the engineering and UX teams to develop the company style guide.
-
-**Tech:** Drupal, Javascript, nginx, Node.js, PHP, Spine.js`,
+        accomplishments: [],
+        description: 'My responsibilities at Great Wolf included maintaining and upgrading the company website and working with the engineering and UX teams to develop the company style guide.',
         endDate: 'February 2014',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'September 2013',
+        tech: [
+          'Drupal',
+          'Javascript',
+          'nginx',
+          'Node.js',
+          'PHP',
+          'Spine.js',
+        ],
         title: 'Web Developer II',
       },
     ],
@@ -177,32 +307,39 @@ const data = [
     id: uuid(),
     positions: [
       {
-        description: `In my second web developer role at AmFam, I was shifted to the Life Insurance team to build a new internal website to help gamify Life Insurance sales.
-
-**Accomplishments:**
-* Developed a REST API in VB.NET that collected data from all across the internal network
-* Developed a robust gamified website complete with charts, graphs, leaderboards, and achievements
-
-**Tech:** CSS, Javascript, raphael.js, VB.NET`,
+        accomplishments: [
+          'Developed a REST API in VB.NET that collected data from all across the internal network',
+          'Developed a robust gamified website complete with charts, graphs, leaderboards, and achievements',
+        ],
+        description: 'On the Life Insurance team, I built an internal website to help gamify Life Insurance sales.',
         endDate: 'September 2013',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'April 2013',
+        tech: [
+          'CSS',
+          'Javascript',
+          'raphael.js',
+          'VB.NET',
+        ],
         title: 'Web Developer II',
       },
 
       {
-        description: `In my first web developer role at AmFam, my directive was to determine accessibility best practices. I worked with differently-abled people across the spectrum, as well as with various alternative input devices (screen readers, keyboard alternatives, voice assistants, etc), to test our application.
-
-**Accomplishments:**
-* Developed a WAI-ARIA quick reference guide
-* Developed the company's first accessibility-first style guide
-
-**Tech:** CSS, Javascript, PHP`,
+        accomplishments: [
+          'Developed a WAI-ARIA quick reference guide',
+          'Developed the company\'s first accessibility-first style guide',
+        ],
+        description: 'At AmFam, my directive was to determine accessibility best practices. I worked with differently-abled people across the spectrum, as well as with various alternative input devices (screen readers, keyboard alternatives, voice assistants, etc), to test our application.',
         endDate: 'April 2013',
         id: uuid(),
         location: 'Madison, WI',
         startDate: 'August 2012',
+        tech: [
+          'CSS',
+          'Javascript',
+          'PHP',
+        ],
         title: 'Web Developer',
       },
     ],
@@ -216,6 +353,7 @@ const data = [
     id: uuid(),
     positions: [
       {
+        accomplishments: [],
         description: `* Participated in daily scrum, review, and retrospective meetings.
 * Utilized Javascript, jQuery, and CSS to create a rich user experience.
 * Took an animated client on-boarding video about the company from concept to completion.`,
@@ -223,6 +361,7 @@ const data = [
         id: uuid(),
         location: 'Janesville, WI',
         startDate: 'June 2011',
+        tech: [],
         title: 'Front End Developer',
       },
     ],
@@ -236,6 +375,7 @@ const data = [
     id: uuid(),
     positions: [
       {
+        accomplishments: [],
         description: `* Participated in weekly status meetings to present my progress and refine project goals.
 * Reviewed business needs to assist the business team in delivering clear and concise goals for the website.
 * Built the company's public-facing website.
@@ -246,6 +386,7 @@ const data = [
         endDate: 'June 2011',
         location: 'Paducah, KY',
         startDate: 'July 2009',
+        tech: [],
         title: 'Front End Developer',
       },
     ],
@@ -265,7 +406,7 @@ const Experience = () => (
         </header>
 
         {/* eslint-disable-next-line no-magic-numbers */}
-        <p>As a software engineer for over {moment(new Date('1 1 1999')).fromNow(true)}, I've got a pretty extensive resume. Unfortunately, hiring managers are rarely interested in long resumes. To account for that, I keep a full list of my work experience right here on my website. If you're looking for your next Front End Developer, Developer Advocate, or Accessibility Engineer, make sure to connect. <span aria-label="Grinning face emoji" role="img">😁</span></p>
+        <p>As a software engineer for over {moment(new Date('January 1, 1999')).fromNow(true)}, I've got an extensive resume. However, not everybody wants to spend hours catching up on my career. Instead, I keep a full list of my work experience right here on my website. If you're looking for your next Front End Developer, Developer Advocate, or Accessibility Engineer, make sure to connect. <span aria-label="Grinning face emoji" role="img">😁</span></p>
       </div>
     </section>
 
@@ -288,16 +429,35 @@ const Experience = () => (
           <ol>
             {positions.map(position => {
               const {
-                description,
+                accomplishments,
                 endDate,
                 location,
                 startDate,
+                tech,
                 title,
               } = position
 
+              let { description } = position
+
+              if (accomplishments?.length) {
+                if (description) {
+                  description += '\n\n'
+                }
+
+                description += `**Accomplishments:**\n* ${accomplishments.join('\n* ')}`
+              }
+
+              if (tech?.length) {
+                if (description) {
+                  description += '\n\n'
+                }
+
+                description += `**Tech:** ${tech.join(', ')}`
+              }
+
               return (
                 <li key={position.id}>
-                  <h3>{title}</h3>
+                  <h4>{title}</h4>
 
                   <div className="meta">
                     <span>{location}</span>
