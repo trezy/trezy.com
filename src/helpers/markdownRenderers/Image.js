c6e4c5d7e4efb28529864c1a1577fd4f39fe5d18
Trezy <tre@trezy.com>
2020-11-12 17:15:52 -0600

    add support for images as figures in Markdown

--- src/helpers/markdownRenderers/Image.js
+++ src/helpers/markdownRenderers/Image.js
@@ -9,31 +9,60 @@ import React from 'react'
 const Image = props => {
 	const {
 		alt,
-		title,
+		node,
 		src,
+		title,
 	} = props
+	const {
+		caption,
+		className,
+		figure,
+		id,
+	} = node.data || {}
+
+	const passableProps = {
+		...props,
+		className,
+		id,
+	}
+	delete passableProps.node
 
 	if (!alt && !title) {
 		console.error('Images must have an alt and/or title attribute.')
 	}
 
+	if (figure) {
+		return (
+			<figure>
+				{Boolean(caption) && (
+					<figcaption>{caption}</figcaption>
+				)}
+
+				<img
+					{...passableProps}
+					alt={alt || title}
+					title={title || alt} />
+			</figure>
+		)
+	}
+
 	return (
 		<img
+			{...passableProps}
 			alt={alt || title}
-			src={src}
 			title={title || alt} />
 	)
 }
 
 Image.defaultProps = {
 	alt: null,
-	// originalNode: null,
+	node: null,
 	title: null,
 }
 
 Image.propTypes = {
 	alt: PropTypes.string,
-	// originalNode: PropTypes.object,
+	node: PropTypes.object,
 	src: PropTypes.string.isRequired,
 	title: PropTypes.string,
 }
