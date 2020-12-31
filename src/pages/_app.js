ae1afbc4d8f9e601db5407243f43e02ac58ad2d1
Trezy <tre@trezy.com>
2020-12-31 00:58:14 -0600

    update font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -56,12 +56,12 @@ function App(props) {
 					<ProfilesContextProvider>
 						<ArticlesContextProvider>
 							<Provider store={store}>
-								<div role="application">
+								<div id="application-wrapper">
 									<NextHead>
 										<meta name="viewport" content="initial-scale=1.0, viewport-fit=cover, width=device-width" />
 
 										<link
-											href="https://fonts.googleapis.com/css?family=Source+Code+Pro&amp;display=swap"
+											href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
 											rel="stylesheet" />
 									</NextHead>
 
