c3695413925e6c37d96dee7eeea567f9dd1ff1d8
Trezy <tre@trezy.com>
2021-01-04 13:53:15 -0600

    fix server-side diagram encoding

--- src/helpers/encodeDiagram.js
+++ src/helpers/encodeDiagram.js
@@ -7,7 +7,13 @@ import { deflate } from 'pako'
 
 export function encodeDiagram(string) {
 	const compressedString = deflate(string, { level: 9, to: 'string' })
-	const base64String = btoa(String.fromCharCode.apply(null, compressedString))
+	let base64String = null
+
+	if (typeof window === 'undefined') {
+		base64String = Buffer.from(compressedString.buffer).toString('base64')
+	} else {
+		base64String = btoa(String.fromCharCode.apply(null, compressedString))
+	}
 
 	return base64String
 		.replace(/\+/g, '-')
