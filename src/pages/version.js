ecc3adf27cbc3279f02d6677224f8c6660b6c1c9
Trezy <tre@trezy.com>
2021-01-15 13:58:55 -0600

    major upgrade to the version page

--- src/pages/version.js
+++ src/pages/version.js
@@ -16,34 +16,74 @@ import PageWrapper from 'components/PageWrapper'
 export default function Version(props) {
 	const {
 		builtAt,
-		commitLink,
-		nextVersion,
-		nodeVersion,
-		repoLink,
-		yarnVersion,
+		environment,
+		repo,
 	} = props
+
+	const authorLink = `https://github.com/${repo.commit.author.username}`
+	const ownerLink = `https://github.com/${repo.owner}`
+	const repoLink = `${ownerLink}/${repo.slug}`
+	const commitLink = `${repoLink}/commit/${repo.commit.hash}`
 
 	return (
 		<PageWrapper title="Version Information">
-			<section className="hero">
+			<section className="block">
+				<header>
+					<h3>Environment</h3>
+				</header>
+
 				<dl>
-					<dt>Built:</dt>
+					<dt>Node Version</dt>
+					<dd>v{environment.node.version}</dd>
+
+					<dt>Next.js Version</dt>
+					<dd>v{environment.next.version}</dd>
+
+					<dt>Built</dt>
 					<dd>{(new Date(builtAt)).toString()}</dd>
+				</dl>
+			</section>
 
-					<dt>Node Version:</dt>
-					<dd>{nodeVersion}</dd>
+			<section className="block">
+				<header>
+					<h3>Git</h3>
+				</header>
 
-					<dt>Yarn Version:</dt>
-					<dd>{yarnVersion}</dd>
+				<dl>
+					<dt>Commit</dt>
+					<dd>
+						<a href={commitLink}>{repo.commit.shortHash}</a>
+						{': '}
+						{repo.commit.message}
+						<br />
+						{Boolean(repo.commit.author.username) && (
+							<small>
+								{'Authored by '}
+								<a href={authorLink}>{repo.commit.author.name || repo.commit.author.username}</a>
+							</small>
+						)}
+					</dd>
 
-					<dt>Next.js Version:</dt>
-					<dd>{nextVersion}</dd>
+					<dt>Repo</dt>
+					<dd>
+						{(repo.owner && repo.slug) && (
+							<>
+								<a href={ownerLink}>{repo.owner}</a>
+								{'/'}
+								<a href={repoLink}>{repo.slug}</a>
+								<br />
+							</>
+						)}
 
-					<dt>Repo:</dt>
-					<dd><a href={repoLink}>{repoLink}</a></dd>
+						{(!repo.owner || !repo.slug) && (
+							<>Unknown<br /></>
+						)}
 
-					<dt>Commit:</dt>
-					<dd><a href={commitLink}>{commitLink}</a></dd>
+
+						<small>
+							Branch: {repo.branch}
+						</small>
+					</dd>
 				</dl>
 			</section>
 		</PageWrapper>
@@ -51,19 +91,90 @@ export default function Version(props) {
 }
 
 export async function getStaticProps() {
-	const [, yarnVersion, nodeVersion, buildEnvironment] = (/yarn\/([\d\.]+) npm\/\? node\/v([\d\.]+ (.*))/.exec(process.env.npm_config_user_agent || '') || [])
+	const [
+		fs,
+		git,
+		gitRev,
+		path,
+		{ promisify },
+		yaml,
+	] = await Promise.all([
+		import('fs'),
+		import('isomorphic-git'),
+		import('git-rev-promises'),
+		import('path'),
+		import('util'),
+		import('yaml'),
+	])
+
+	const [,
+		yarnVersion = '',
+	] = (/yarn\/(v?\d+\.\d+\.\d+)/.exec(process.env.npm_config_user_agent || '') || [])
 
 	const repoLink = `https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}`
 
+	const asyncReadFile = promisify(fs.readFile)
+	const yarnLockPath = path.resolve(process.cwd(), 'yarn.lock')
+	const lockfile = await asyncReadFile(yarnLockPath, 'utf8')
+	const dependencies = yaml.parse(lockfile)
+
+	const nextDependencyKey = Object
+		.keys(dependencies)
+		.find(dependency => /^next@.*:.*\d+\.\d+\.\d+$/iu.exec(dependency))
+	const nextVersion = dependencies[nextDependencyKey]?.version
+
+	const [
+		gitBranch,
+		gitCommitHash,
+		gitCommitShortHash,
+		gitCommitMessage,
+	] = await Promise.all([
+		gitRev.branch(),
+		gitRev.long(),
+		gitRev.short(),
+		gitRev.message(),
+	])
+	const [
+		[{
+			commit: {
+				author: {
+					name: gitCommitAuthorName,
+				}
+			},
+		}]
+	] = await Promise.all([
+		await git.log({
+			depth: 1,
+			dir: process.cwd(),
+			fs,
+		}),
+	])
+
 	return {
 		props: {
-			buildEnvironment,
 			builtAt: Date.now(),
-			commitLink: `${repoLink}/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`,
-			nextVersion: '?', //process.env.npm_package_dependencies_next,
-			nodeVersion,
-			repoLink,
-			yarnVersion,
+			environment: {
+				dependencies,
+				deploymentType: process.env.VERCEL_ENV || '',
+				next: { version: nextVersion },
+				node: { version: process.version.replace(/^v/iu, '') },
+				platform: process.platform,
+				region: process.env.VERCEL_REGION || '',
+			},
+			repo: {
+				branch: process.env.VERCEL_GIT_COMMIT_REF || gitBranch,
+				commit: {
+					author: {
+						name: process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME || gitCommitAuthorName,
+						username: process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN || '',
+					},
+					hash: process.env.VERCEL_GIT_COMMIT_SHA || gitCommitHash,
+					shortHash: process.env.VERCEL_GIT_COMMIT_SHA || gitCommitShortHash,
+					message: process.env.VERCEL_GIT_COMMIT_MESSAGE || gitCommitMessage,
+				},
+				owner: process.env.VERCEL_GIT_REPO_OWNER || '',
+				slug: process.env.VERCEL_GIT_REPO_SLUG || '',
+			},
 		},
 	}
 }
