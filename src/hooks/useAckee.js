f55c18c9e479a1949aa5e55a137326551119bfbb
Trezy <t@trezy.com>
2022-04-05 11:07:59 -0500

    force Ackee to operate as a singleton

--- src/hooks/useAckee.js
+++ src/hooks/useAckee.js
@@ -1,9 +1,5 @@
 // Module imports
-import {
-	useCallback,
-	useEffect,
-	useState,
-} from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as ackee from 'ackee-tracker'
 
@@ -11,19 +7,59 @@ import * as ackee from 'ackee-tracker'
 
 
 
-// Variables
-let ackeeInstance = null
+class Ackee {
+	/****************************************************************************\
+	 * Private static properties
+	\****************************************************************************/
+
+	static #domainID = null
+
+	static #instance = null
+
+	static #isInitialised = false
+
+	static #previousPath = null
+
+	static #url = null
 
 
 
 
 
-function createRecord() {
-	ackeeInstance.record(process.env.NEXT_PUBLIC_ACKEE_DOMAIN_ID, {
-		...ackee.attributes(),
-		siteLocation: window.location.href,
-		siteReferrer: document.referrer,
-	})
+	/****************************************************************************\
+	 * Private static methods
+	\****************************************************************************/
+
+	static initialise(domainID, url) {
+		if (!Ackee.#isInitialised) {
+			Ackee.#domainID = domainID
+			Ackee.#url = url
+
+			if (typeof window !== 'undefined') {
+				Ackee.#instance = ackee.create(Ackee.#url, {
+					detailed: false,
+					ignoreLocalhost: false,
+					ignoreOwnVisits: false,
+				})
+			}
+
+			Ackee.#isInitialised = true
+		}
+	}
+
+	static record(path) {
+		if (Ackee.#previousPath !== path) {
+			const siteLocation = new URL(path, window.location.origin)
+
+			Ackee.#previousPath = path
+
+			Ackee.#instance.record(Ackee.#domainID, {
+				...ackee.attributes(),
+				siteLocation,
+				siteReferrer: document.referrer,
+			})
+		}
+	}
 }
 
 
@@ -33,16 +69,13 @@ function createRecord() {
 export function useAckee() {
 	const Router = useRouter()
 
-	if (!ackeeInstance && (typeof window !== 'undefined')) {
-		ackeeInstance = ackee.create(process.env.NEXT_PUBLIC_ACKEE_URL, {
-			detailed: false,
-			ignoreLocalhost: false,
-			ignoreOwnVisits: false,
-		})
-	}
-
 	useEffect(() => {
-		Router.events.on('routeChangeComplete', createRecord)
-		return () => Router.events.off('routeChangeComplete', createRecord)
+		Ackee.initialise(process.env.NEXT_PUBLIC_ACKEE_DOMAIN_ID, process.env.NEXT_PUBLIC_ACKEE_URL)
+		Router.events.on('routeChangeComplete', Ackee.record)
+		return () => Router.events.off('routeChangeComplete', Ackee.record)
 	}, [])
+
+	return {
+		'function': () => {},
+	}
 }
