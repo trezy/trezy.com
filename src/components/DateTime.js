0fa3267e608d5596ed45aff9f675a2bc9d6e7772
Trezy <t@trezy.com>
2022-08-09 12:15:23 -0500

    significantly expand the functionality and validation of the `<DateTime>` component (previously `<Time>`)

---
+++ src/components/DateTime.js
@@ -0,0 +1,222 @@
+// Module imports
+import {
+	useEffect,
+	useMemo,
+	useState,
+} from 'react'
+import PropTypes from 'prop-types'
+import { useRouter } from 'next/router'
+
+
+
+
+
+export function DateTime(props) {
+	const {
+		dateFormat,
+		separator,
+		showDate,
+		showTime,
+		timeFormat,
+		titlePrefix,
+		value,
+	} = props
+
+	const [locale, setLocale] = useState('en-US')
+	const Router = useRouter()
+
+	const valueAsDate = useMemo(() => {
+		if (value instanceof Date) {
+			return value
+		}
+
+		return new Date(value)
+	}, [value])
+
+	const formattedValue = useMemo(() => {
+		const pieces = []
+
+		if (showTime) {
+			const timeFormatter = new Intl.DateTimeFormat(locale, timeFormat)
+			pieces.push(timeFormatter.format(valueAsDate))
+		}
+
+		if (showDate) {
+			const dateFormatter = new Intl.DateTimeFormat(locale, dateFormat)
+			pieces.push(dateFormatter.format(valueAsDate))
+		}
+
+		return pieces.join(separator)
+	}, [
+		dateFormat,
+		separator,
+		showDate,
+		showTime,
+		locale,
+		timeFormat,
+		valueAsDate,
+	])
+
+	useEffect(() => {
+		setLocale(Router.locale)
+	}, [
+		Router.locale,
+		setLocale,
+	])
+
+	return (
+		<time
+			dateTime={valueAsDate.toISOString()}
+			title={`${titlePrefix} ${valueAsDate.toUTCString()}`.trim()}>
+			{formattedValue}
+		</time>
+	)
+}
+
+DateTime.defaultProps = {
+	dateFormat: {
+		dateStyle: 'long',
+	},
+	separator: ' - ',
+	showDate: true,
+	showTime: true,
+	timeFormat: {
+		timeStyle: 'short',
+	},
+	titlePrefix: '',
+}
+
+DateTime.propTypes = {
+	dateFormat: PropTypes.shape({
+		calendar: PropTypes.oneOf([
+			'buddhist',
+			'chinese',
+			'coptic',
+			'dangi',
+			'ethioaa',
+			'ethiopic',
+			'gregory',
+			'hebrew',
+			'indian',
+			'islamic',
+			'islamic-umalqura',
+			'islamic-tbla',
+			'islamic-civil',
+			'islamic-rgsa',
+			'iso8601',
+			'japanese',
+			'persian',
+			'roc',
+			'islamicc',
+		]),
+		dateStyle: PropTypes.oneOf([
+			'long',
+			'medium',
+			'short',
+		]),
+		day: PropTypes.oneOf([
+			'2-digit',
+			'numeric',
+		]),
+		era: PropTypes.oneOf([
+			'long',
+			'narrow',
+			'short',
+		]),
+		month: PropTypes.oneOf([
+			'2-digit',
+			'long',
+			'narrow',
+			'numeric',
+			'short',
+		]),
+		numberingSystem: PropTypes.oneOf([
+			'arab',
+			'arabext',
+			'bali',
+			'beng',
+			'deva',
+			'fullwide',
+			' gujr',
+			'guru',
+			'hanidec',
+			'khmr',
+			' knda',
+			'laoo',
+			'latn',
+			'limb',
+			'mlym',
+			'mong',
+			'mymr',
+			'orya',
+			'tamldec',
+			'telu',
+			'thai',
+			'tibt',
+		]),
+		weekday: PropTypes.oneOf([
+			'long',
+			'narrow',
+			'short',
+		]),
+		year: PropTypes.oneOf([
+			'2-digit',
+			'numeric',
+		]),
+	}),
+	separator: PropTypes.string,
+	showDate: PropTypes.bool,
+	showTime: PropTypes.bool,
+	timeFormat: PropTypes.shape({
+		dayPeriod: PropTypes.oneOf([
+			'long',
+			'narrow',
+			'short',
+		]),
+		fractionalSecondDigits: PropTypes.oneOf([
+			0,
+			1,
+			2,
+			3,
+		]),
+		hour: PropTypes.oneOf([
+			'2-digit',
+			'numeric',
+		]),
+		hour12: PropTypes.bool,
+		hourCycle: PropTypes.oneOf([
+			'h11',
+			'h12',
+			'h23',
+			'h24',
+		]),
+		minute: PropTypes.oneOf([
+			'2-digit',
+			'numeric',
+		]),
+		second: PropTypes.oneOf([
+			'2-digit',
+			'numeric',
+		]),
+		timeStyle: PropTypes.oneOf([
+			'full',
+			'long',
+			'medium',
+			'short',
+		]),
+		timeZone: PropTypes.string,
+		timeZoneName: PropTypes.oneOf([
+			'long',
+			'longGeneric',
+			'longOffset',
+			'short',
+			'shortGeneric',
+			'shortOffset',
+		]),
+	}),
+	titlePrefix: PropTypes.string,
+	value: PropTypes.oneOfType([
+		PropTypes.number,
+		PropTypes.string,
+	]).isRequired,
+}
