ee27b7a3f214b43d653f0db419a9ff6d85bc2da6
Trezy <t@trezy.com>
2022-08-09 10:57:20 -0500

    move static head tags to document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,6 @@ import 'scss/app.scss'
 import { AnimatePresence } from 'framer-motion'
 import { ColorModeContextProvider } from 'react-color-mode'
 import { config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core'
-import NextHead from 'next/head'
 import { Provider as LyketProvider } from '@lyket/react'
 import { useRouter } from 'next/router'
 
@@ -62,14 +61,6 @@ export default function App(props) {
 						<ProfilesContextProvider>
 							<BannerContextProvider>
 								<div id="application-wrapper">
-									<NextHead>
-										<meta name="viewport" content="initial-scale=1.0, viewport-fit=cover, width=device-width" />
-
-										<link
-											href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-											rel="stylesheet" />
-									</NextHead>
-
 									<Banner isServer={isServer} />
 
 									<AnimatePresence
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,6 +15,8 @@ function Document() {
 	return (
 		<Html>
 			<Head>
+				<meta name="viewport" content="initial-scale=1.0, viewport-fit=cover, width=device-width" />
+
 				<meta name="application-name" content="Trezy.com" />
 				<meta name="theme-color" content="#0092c7" />
 
@@ -27,6 +29,10 @@ function Document() {
 
 				<link rel="webmention" href="https://webmention.io/trezy.com/webmention" />
 				<link rel="pingback" href="https://webmention.io/trezy.com/xmlrpc" />
+
+				<link
+					href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+					rel="stylesheet" />
 
 				<ColorModeScript />
 			</Head>
