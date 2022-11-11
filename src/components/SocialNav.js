3de099a23539e960e0f4b21a2d5b878a9310e329
Trezy <t@trezy.com>
2022-11-11 11:09:42 -0600

    add Mastodon

--- src/components/SocialNav.js
+++ src/components/SocialNav.js
@@ -6,8 +6,8 @@ import {
 	faGithub,
 	faInstagram,
 	faLinkedin,
+	faMastodon,
 	faNpm,
-	faPatreon,
 	faSoundcloud,
 	faSpeakerDeck,
 	faTwitter,
@@ -44,79 +44,79 @@ function SocialNav(props) {
 
 	return (
 		<Nav
-			className="social"
+			className={'social'}
 			isOpen={isOpen}>
 			<NavLink
 				{...defaultOptions}
+				icon={faMastodon}
+				title={'Mastodon'}
+				href={'https://mastodon.social/TrezyCodes'} />
+
+			<NavLink
+				{...defaultOptions}
 				icon={faTwitter}
-				title="Twitter"
-				href="https://twitter.com/TrezyCodes" />
+				title={'Twitter'}
+				href={'https://twitter.com/TrezyCodes'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faInstagram}
-				title="Instagram"
-				href="https://instagram.com/TrezyCodes" />
+				title={'Instagram'}
+				href={'https://instagram.com/TrezyCodes'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faLinkedin}
-				title="LinkedIn"
-				href="https://linkedin.com/in/trezy" />
+				title={'LinkedIn'}
+				href={'https://linkedin.com/in/trezy'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faYoutube}
-				title="YouTube"
-				href="/youtube" />
+				title={'YouTube'}
+				href={'/youtube'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faDiscord}
-				title="Discord"
-				href="/discord" />
-
-			<NavLink
-				{...defaultOptions}
-				icon={faPatreon}
-				title="Patreon"
-				href="https://www.patreon.com/trezy" />
+				title={'Discord'}
+				href={'/discord'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faGithub}
-				title="Github"
-				href="https://github.com/trezy" />
+				title={'Github'}
+				href={'https://github.com/trezy'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faCodepen}
-				title="Codepen"
-				href="https://codepen.io/trezy" />
+				title={'Codepen'}
+				href={'https://codepen.io/trezy'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faNpm}
-				title="npm"
-				href="https://npmjs.com/~trezy" />
+				title={'npm'}
+				href={'https://npmjs.com/~trezy'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faDev}
-				title="DEV Community"
-				href="https://dev.to/trezy" />
+				title={'DEV Community'}
+				href={'https://dev.to/trezy'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faSpeakerDeck}
-				title="SpeakerDeck"
-				href="https://speakerdeck.com/trezy" />
+				title={'SpeakerDeck'}
+				href={'https://speakerdeck.com/trezy'} />
 
 			<NavLink
 				{...defaultOptions}
 				icon={faSoundcloud}
-				title="SoundCloud"
-				href="https://soundcloud.com/TrezyCodes" />
+				title={'SoundCloud'}
+				href={'https://soundcloud.com/TrezyCodes'} />
 		</Nav>
 	)
 }
